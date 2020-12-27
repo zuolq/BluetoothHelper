533262702f31c88ca50e744b77c510abe06d62ea
zuolq <13543416503@163.com>
2020-12-27 17:48:04 +0800

    dev第二次修改

--- app/src/main/java/top/wuhaojie/bluetoothhelper/MainActivity.java
+++ app/src/main/java/top/wuhaojie/bluetoothhelper/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("MainActivity", "---------------------这是dev分支-------------------------------");
+        Log.e("MainActivity", "---------------------这是dev分支第二次修改-------------------------------");
         btHelperClient = BtHelperClient.from(MainActivity.this);
 
         btHelperClient.requestEnableBt();
